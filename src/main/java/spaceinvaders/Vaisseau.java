d6ad15b85772ec4e3919f043a0d39caadf9056f7
Joffrey Guillon <Joffrey Guillon@192.168.1.13>
2017-05-06 17:59:11 +0200

    Mis à jour fin de l'étape 1 du Sprint 2

--- src/main/java/spaceinvaders/Vaisseau.java
+++ src/main/java/spaceinvaders/Vaisseau.java
@@ -4,14 +4,42 @@ public class Vaisseau {
 	
 	int x;
 	int y;
-
-	public Vaisseau(int x, int y) {
+	int longueur;
+	int hauteur;
+	
+	public Vaisseau(int longueur, int hauteur) {
+		this(longueur, hauteur, 0, 0);
+	}
+	
+	public Vaisseau(int longueur,int hauteur,int x, int y) {
+		this.longueur=longueur;
+		this.hauteur=hauteur;
 		this.x = x;
 		this.y = y;
 	}
 
 	public boolean occupeLaPosition(int x, int y) {
-		return (this.x==x) && (this.y==y);
+	     return (estAbscisseCouverte(x) && estOrdonneeCouverte(y));
+    }
+
+	public boolean estOrdonneeCouverte(int y) {
+		return (ordonnéeLaPlusBasse()<=y) && (y<=ordonnéeLaPlusHaute());
+	}
+
+	public int ordonnéeLaPlusHaute() {
+		return this.y;
+	}
+
+	public int ordonnéeLaPlusBasse() {
+		return ordonnéeLaPlusHaute()-this.hauteur+1;
+	}
+
+	public boolean estAbscisseCouverte(int x) {
+		return (abscisseLaPlusAGauche()<=x) && (x<=abscisseLaPlusADroite());
+	}
+
+	public int abscisseLaPlusADroite() {
+		return this.x+this.longueur-1;
 	}
 
 	public void seDeplacerVersLaDroite() {
@@ -19,11 +47,17 @@ public class Vaisseau {
 		
 	}
 	
-	public int abscisse() {
+	public int abscisseLaPlusAGauche() {
         return this.x;
 	}
 
 	public void seDeplacerVersLaGauche() {
 		      this.x = this.x - 1 ;
+	}
+
+	public void positionner(int x, int y) {
+		this.x=x;
+		this.y=y;
+		
 	}
 }
